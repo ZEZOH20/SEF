0ebfba1d6b7dba1ea4f3158d35dbdd2dd733b003
zeyad <zieadhammad15@gmail.com>
2023-05-17 19:59:33 +0300

    responsive

--- bootstrap-task/js/script.js
+++ bootstrap-task/js/script.js
@@ -1,33 +1,4 @@
 
-
-//    var allProducts = document.querySelectorAll(".list  li")
-//    var content = document.querySelector("#content")
-//    var btn = document.querySelector("#btn1")
-//    var totalPrice = 0
-
-
-//    allProducts.forEach( function (item){
-//      item.onclick = function (){
-
-//          totalPrice +=   +(item.getAttribute("price"))
-//          content.innerHTML  +=  item.textContent + " "
-
-//          if ( content.innerHTML != ""){
-//             btn.style.display = "block"
-//             btn.style.color = "white"
-//             btn.style.backgroundColor = "green"
-//             btn.style.width = "200px"
-//             btn.style.height = "45px"
-//          }
-//      }
-//    })
-
-//    btn.onclick = function (){
-//     console.log(totalPrice)
-//    }
-
-
-
 var allButtons = document.querySelectorAll("#button")
 var content = document.querySelector("#content")
 allButtons.forEach(function(item){
@@ -38,7 +9,23 @@ allButtons.forEach(function(item){
         content.innerHTML = totalPrice 
         console.log(totalPrice)
     }
+});
+
+// remove-add slider
+var elements = document.querySelectorAll('.carousel-item')
+var bg = document.querySelector('.main-background');
+console.log(elements)
+window.addEventListener('resize' , function(){
+    console.log(window.innerWidth)
+    elements.forEach(function(e){
+        if(this.window.innerWidth < 1200){
+            e.classList.remove('carousel-item')
+        }else{
+            e.classList.add('carousel-item')
+        }
+    })
 })
+// remove-add slider
 
 
 
